9c76f014a1c2d1d5fd04201270849506715b1977
Venkatakrishnan Ramesh <venkatkrishnan2003@gmail.com>
2023-03-12 14:00:27 +0530

    search functionality added

--- Task 2/Advance design/script.js
+++ Task 2/Advance design/script.js
@@ -1,17 +1,68 @@
-const userTable = document.querySelector("#userTable tbody");
+const searchInput = document.getElementById('searchInput');
+const userData = document.getElementById('userData').tBodies[0].rows;
 
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch('https://jsonplaceholder.typicode.com/users')
 	.then(response => response.json())
-	.then(users => {
-		users.forEach(user => {
-			const row = document.createElement("tr");
-			row.innerHTML = `
-				<td>${user.id}</td>
-				<td>${user.name}</td>
-				<td>${user.username}</td>
-				<td>${user.email}</td>
-			`;
-			userTable.appendChild(row);
+	.then(data => {
+		data.forEach(user => {
+			const row = document.createElement('tr');
+			const id = document.createElement('td');
+			const name = document.createElement('td');
+			const username = document.createElement('td');
+			const email = document.createElement('td');
+			
+			id.textContent = user.id;
+			name.textContent = user.name;
+			username.textContent = user.username;
+			email.textContent = user.email;
+			
+			row.appendChild(id);
+			row.appendChild(name);
+			row.appendChild(username);
+			row.appendChild(email);
+			
+			document.getElementById('userData').appendChild(row);
 		});
 	})
-	.catch(error => console.log(error));
+	.catch(error => console.error(error));
+
+searchInput.addEventListener('keyup', function() {
+	const searchTerm = searchInput.value.toLowerCase();
+	
+	for (let i = 0; i < userData.length; i++) {
+		let found = false;
+		const cells = userData[i].cells;
+		
+		for (let j = 0; j < cells.length; j++) {
+			const cellValue = cells[j].textContent.toLowerCase();
+			
+			if (cellValue.includes(searchTerm)) {
+				found = true;
+				break;
+			}
+		}
+		
+		if (found) {
+			userData[i].style.display = '';
+		} else {
+			userData[i].style.display = 'none';
+		}
+	}
+});
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen(); 
+    }
+  }
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'F11') {
+    event.preventDefault();
+    toggleFullScreen();
+  }
+});
